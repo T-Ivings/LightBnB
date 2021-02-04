ba1bf764fa726e2d94aa2682f1c236017d53c940
Taylor <ivingstaylor@gmail.com>
2021-02-04 21:02:32 +0000

    resevations shown

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -20,9 +20,8 @@ const getUserWithEmail = function(email) {
   return pool.query(`
   SELECT * FROM users
   WHERE email = $1;`, [email])
-  .then(res => {
-    return Promise.resolve(res.rows[0])
-})};
+  .then(res => res.rows[0]);
+};
 
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,9 +34,9 @@ const getUserWithId = function(id) {
   return pool.query(`
   SELECT * FROM users
   WHERE id = $1;`, [id])
-  .then(res => {
-    return Promise.resolve(res.rows[0])
-})};
+  .then(res => res.rows[0]);
+
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -51,11 +50,8 @@ const addUser =  function(user) {
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
   RETURNING *;`, [user.name, user.email, user.password])
-  .then(res => {
-    return Promise.resolve(res.row[0]);
-  })
-  return Promise.resolve(user);
-}
+  .then(res => res.row[0]);
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -66,8 +62,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return query.pool(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then(res => res.rows);
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -81,7 +87,7 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
   SELECT * FROM properties
-  LIMIT $1
+  LIMIT $1;
   `, [limit])
   .then(res => res.rows);
 }
