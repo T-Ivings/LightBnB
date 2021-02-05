bebb39ef2d32f5a5598a59b25b7a24cb2051839d
Taylor <ivingstaylor@gmail.com>
2021-02-05 02:36:46 +0000

    create properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -94,54 +94,62 @@ const getAllProperties = function(options, limit = 10) {
   `;
 
   // 3
-  if (options.city) {
-    queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `WHERE properties.owner_id = $${queryParams.length}
+    `
   }
 
-  if (options.owner_id && queryParams.length === 0) {
-    queryParams.push(options.owner_id);
-    queryString += `WHERE owner_id IS $${queryParams.length}`
-  } else if (options.owner_id) {
-    queryParams.push(options.owner_id);
-    queryString += `AND owner_id IS $${queryParams.length}`
+  if (options.city && queryParams === 0) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}
+    `;
+  } else if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length}
+    `;
   }
+
+
 
   if (options.minimum_price_per_night && queryParams.length === 0) {
     queryParams.push(options.minimum_price_per_night + '00');
-    queryString += `WHERE cost_per_night >= $${queryParams.length}`
+    queryString += `WHERE cost_per_night >= $${queryParams.length}
+    `
   } else if (options.minimum_price_per_night) {
     queryParams.push(options.minimum_price_per_night + '00');
-    queryString += ` AND cost_per_night >= $${queryParams.length}`
+    queryString += ` AND cost_per_night >= $${queryParams.length}
+    `
   }
 
   if (options.maximum_price_per_night && queryParams.length === 0) {
     queryParams.push(options.maximum_price_per_night + '00');
-    queryString += `WHERE cost_per_night <= $${queryParams.length}`
+    queryString += `WHERE cost_per_night <= $${queryParams.length}
+    `
   } else if (options.maximum_price_per_night) {
     queryParams.push(options.maximum_price_per_night + '00');
-    queryString += ` AND cost_per_night <= $${queryParams.length}`
+    queryString += ` AND cost_per_night <= $${queryParams.length}
+    `
   }
 
-  if(options.minimum_rating && queryParams.length === 0) {
+  queryString += `GROUP BY properties.id
+  `
+
+  if(options.minimum_rating) {
     queryParams.push(options.minimum_rating);
-    queryString += `WHERE property_reviews.rating >= $${queryParams.length}`
-  } else if (options.minimum_rating) {
-    queryParams.push(options.minimum_rating);
-    queryString += `AND property_reviews.rating >= $${queryParams.length}`
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}
+    `
   }
-  // 4
-  queryParams.push(limit);
-  queryString += `
-  GROUP BY properties.id
+
+queryParams.push(limit);
+queryString += `
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
-  `;
+  `; 
 
-  // 5
-  console.log(queryString, queryParams);
 
-  // 6
+
   return pool.query(queryString, queryParams)
   .then(res => res.rows);
   
@@ -155,9 +163,12 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+    return pool.query(`
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`, 
+    [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(res => 
+      res.rows)
+  }
 exports.addProperty = addProperty;
